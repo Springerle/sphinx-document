3e97ff1d33b34f2e8100642241e7587b329f97c0
jhermann <jh@web.de>
2020-04-13 12:08:43 +0200

    add fake minimal setup for Invoke

---
+++ {{cookiecutter.repo_name}}/setup.py
@@ -0,0 +1,16 @@
+# fake minimal setup for Invoke
+
+project = dict(
+    name = '{{ cookiecutter.project_name }}',
+    version = '{{ cookiecutter.version }}',
+    author='{{ cookiecutter.full_name }}',
+    author_email='{{ cookiecutter.email }}',
+    license='{{ cookiecutter.license }}',
+    packages = [],
+    url='{{ cookiecutter.github_url }}#readme',
+    description='{{ cookiecutter.short_description }}',
+)
+
+if __name__ == '__main__':
+    import setuptools
+    setuptools.setup(**project)
